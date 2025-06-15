c01f755a8bc4428cc296728afb84b52aa3485325
yagyaadhikari <yagyaadhikari02@gmail.com>
2025-06-15 20:17:29 +0545

    users controller returning users data within index file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +20,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+/* public */
+Route::resource('users',UserController::class);
